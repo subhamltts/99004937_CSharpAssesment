06888efc8848ac321e45ae571dc436d9889d9a89
subhamltts <subham.panda@ltts.com>
2021-07-15 16:28:06 +0530

    commit

---
+++ problemtwo_compilationerror/DirectReport/DirectReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectReport
+{
+    public class DirectReport
+    {
+
+        public Employee ReportsTo { get; set; }
+        public string ProjectCode { get; set; }
+
+        public DirectReport(Employee managerEmpObj, string pCode) : base(eID, eName, eLevel, eDoJ)
+        {
+            ReportsTo = managerEmpObj;
+            ProjectCode = pCode;
+        }
+    }
+}
---
+++ problemtwo_compilationerror/manager/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using employee;
+
+namespace manager
+{
+    public class Manager
+    {
+        public List<employee> DirectReports { get; set; }
+        public string CostCode { get; set; }
+
+        public Manager(List<employee> drEmployeesLst, string cCode) : base(eID, eName, eLevel, eDoJ)
+        {
+            DirectReports = drEmployeesLst;
+            CostCode = cCode;
+
+        }
+
+        public List<string> GetMyReports()
+        {
+            return DirectReports.Select(x => x.EmpName).ToList();
+        }
+    }
+}
